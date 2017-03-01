d22a5f2523116f94c07c8b29dd7eb25cfd8af9b6
Daniel <danielhp@optusnet.com.au>
2017-03-01 17:29:06 +1100

    Cleaned up code, and added more comments

--- code/objects.py
+++ code/objects.py
@@ -3,13 +3,18 @@ import random
 class Ball():
 
     def __init__(self, startX, startY, pygame, surface, radius):
-        self.startPos = (startX, startY)
+        self.startPos = (startX, startY) # This is where the ball will return to after a point is scored
         self.x = startX
         self.y = startY
+
+        # These are so the bats can interact with the game without pygame being
+        # imported or this class having to be in the main file.
         self.pygame = pygame
         self.surface = surface
+
         self.radius = radius
 
+        # Getting a random direction for the ball to start in
         self.dx = random.randint(-1, 1) * 3
         while self.dx == 0:
             self.dx = random.randint(-1, 1) * 3
@@ -21,24 +26,29 @@ class Ball():
 
         self.score = [0,0]
         self.pauseTime = 500 # The amount of time (in milliseconds) that the ball stops for after a point is won
-        self.scoredTime = 0  # The time when the last point was scored
+        self.scoredTime = 0  # The time when the last point was scored; used mainly externally for pausing just the ball
 
         self.scored = False
 
+        # Secret!
         self.nggyu = self.pygame.mixer.Sound("../assets/nggyu.ogg")
         self.cej = self.pygame.mixer.Sound("../assets/cej.ogg")
         self.songs = [self.nggyu, self.cej]
 
-    def playSong(self):
+    def playSong(self): # A suprise for a point is scored (sometimes)
         if random.random() < 0.2:
             random.choice(self.songs).play()
 
+    # Reset the ball after a point is scored
     def resetBall(self):
         self.scored = True
-        self.scoredTime = self.pygame.time.get_ticks()
+        self.scoredTime = self.pygame.time.get_ticks() # Gets the time when a point was scored
+
+        # Reset ball position
         self.x = self.startPos[0]
         self.y = self.startPos[1]
 
+        # Getting a new random direction for the ball to begin in
         self.dx = random.randint(-1, 1) * 3
         while self.dx == 0:
             self.dx = random.randint(-1, 1) * 3
@@ -46,6 +56,7 @@ class Ball():
         while self.dy == 0:
             self.dy = random.randint(-3, 3)
 
+    # Check for collisions with the bats and the edges of the window
     def checkForCollisions(self, windowWidth, windowHeight, leftBat, rightBat):
         # Check For collision with top and bottom boundaries
         if self.y - self.radius < 0 or self.y + self.radius > windowHeight:
@@ -56,34 +67,27 @@ class Ball():
             self.score[1] += 1
             self.resetBall()
             self.playSong()
-            return "left"
 
         # Right side - awards point to left player
         elif self.x + self.radius > windowWidth:
             self.score[0] += 1
             self.resetBall()
             self.playSong()
-            return "right"
 
-        # Left Bat
+        # Left Bat - Note for both bats: the collisions are only with the innermost side.
         if self.x - self.radius < leftBat.x + leftBat.width and leftBat.y < self.y < leftBat.y + leftBat.height:
             self.dx *= -1
-            if leftBat.isMoving:
-                self.dx *= 1.1
-                self.dy *= 1.1
+            self.speedUp()
 
         # Right Bat
         if self.x + self.radius > rightBat.x and rightBat.y < self.y < rightBat.y + rightBat.height:
             self.dx *= -1
-            if rightBat.isMoving:
-                self.dx *= 1.1
-                self.dy *= 1.1
+            self.speedUp()
 
     def move(self, windowWidth, windowHeight, leftBat, rightBat):
-        if not self.scored:
-            self.checkForCollisions(windowWidth, windowHeight, leftBat, rightBat)
-            self.x += self.dx
-            self.y += self.dy
+        self.checkForCollisions(windowWidth, windowHeight, leftBat, rightBat)
+        self.x += self.dx
+        self.y += self.dy
 
     def draw(self, drawAtStartPos):
         if drawAtStartPos:
@@ -92,20 +96,29 @@ class Ball():
             self.pygame.draw.circle(self.surface, self.colour, (int(round(self.x)), int(round(self.y))), self.radius)
             self.scored = False
 
+    # Speeds the ball up if the bat it is colliding with is moving during the collision
+    def speedUp():
+        if leftBat.isMoving or rightBat.isMoving:
+            self.dx *= 1.1
+            self.dy *= 1.1
+
 class Bat(object):
 
     def __init__(self, x, startY, pygame, surface, width, height):
         self.x = x
         self.y = startY
+
+        # These are so the bats can interact with the game without pygame being
+        # imported or this class having to be in the main file.
         self.pygame = pygame
         self.surface = surface
 
         self.width = width
         self.height = height
 
-        self.speed = 2.5
-        self.dy = 0
-        self.isMoving = False
+        self.speed = 2.5 # The speed the bat will do either way
+        self.dy = 0 # The speed the bat is currently doing
+        self.isMoving = False # For the ball to speed up when it collides with a moving bat
 
         self.colour = (255, 255, 255)
 
--- code/pong.py
+++ code/pong.py
@@ -2,61 +2,80 @@ import pygame, sys, os
 import pygame.event as GAME_EVENTS
 import pygame.time as GAME_TIME
 
-import objects
+import objects # This contains the classes necessary for the game
 
+# Window dimensions and derived values
 windowWidth = 800
 windowHeight = 600
 centreX = windowWidth/2
+centreY = windowHeight/2
 
+# Initialize Pygame and the mixer
 pygame.init()
 pygame.mixer.init()
+
+# Initialize the surface for objects to be drawn on
 surface = pygame.display.set_mode((windowWidth, windowHeight))
+
+# Get a font for the score display set up
 scoreFontFile = open(os.path.abspath("../assets/pong_score.ttf"), "r")
 scoreFont = pygame.font.Font(scoreFontFile, 50)
 
+# Set the title of the window
 pygame.display.set_caption("Pong")
 
+# A dictionary with the states of all the control keys (apart from esc) to allow
+# continuous movement
 controlsState = {"leftUp": False, "leftDown": False, "rightUp": False, "rightDown": False}
 
-ball = objects.Ball(windowWidth/2, windowHeight/2, pygame, surface, 15)
-leftBat = objects.Bat(10, windowHeight/2, pygame, surface, 15, 100)
-rightBat = objects.Bat(windowWidth - 25, windowHeight/2, pygame, surface, 15, 100)
+# Initialize the game objects
+ball = objects.Ball(centreX, centreY, pygame, surface, 15)
+leftBat = objects.Bat(10, centreY, pygame, surface, 15, 100)
+rightBat = objects.Bat(windowWidth - 25, centreY, pygame, surface, 15, 100)
 
+# Draw the net (dotted line) down the middle of the window
 def drawNet():
     numberOfLines = 20
     width = 15
     height = windowHeight / (numberOfLines*2)
-    
-    i = height/2
+
+    i = height/2 # i is not initialized to 0 because it is offset to make the line a bit more centred
 
     while i < windowHeight:
         pygame.draw.line(surface, (127,127,127), (centreX, i), (centreX, i+height), width)
         i += 2*height
 
+# Display the score for the users to see during the game
 def drawScore():
     scoreColour = (127,127,127)
-    
+
     p1Score = scoreFont.render(str(ball.score[0]), 1, scoreColour)
     p2Score = scoreFont.render(str(ball.score[1]), 1, scoreColour)
 
     thirdOfWindow = windowWidth//3
     surface.blit(p1Score, (centreX - 100 - scoreFont.size( str(ball.score[1]) )[0] // 2, 50))
     surface.blit(p2Score, (centreX + 100, 50)) # - scoreFont.size( str(ball.score[1]) )[0] // 2
-    
+
+# Quit and uninitialise the game
 def quitGame():
     print(ball.score)
     scoreFontFile.close()
     pygame.quit()
     sys.exit()
 
+# The main loop
 while True:
 
+    # Cover the previous frame in order to avoid smearing and make it appear as
+    # if things were moving
     surface.fill((0, 0, 0))
 
+    # Check for system and user events and act appropriately
     for event in GAME_EVENTS.get():
 
         if event.type == pygame.KEYDOWN:
 
+            # Movement for player 1 or the left bat
             if event.key == pygame.K_w:
                 controlsState["leftUp"] = True
                 controlsState["leftDown"] = False
@@ -64,6 +83,7 @@ while True:
                 controlsState["leftDown"] = True
                 controlsState["leftUp"] = False
 
+            # Movement for player 2 or the right bat
             if event.key == pygame.K_UP:
                 controlsState["rightUp"] = True
                 controlsState["rightDown"] = False
@@ -71,39 +91,46 @@ while True:
                 controlsState["rightDown"] = True
                 controlsState["rightUp"] = False
 
+            # Quit if the user hits the escape key
             if event.key == pygame.K_ESCAPE:
                 quitGame()
 
+        # Stop movement when a key is no longer being held down
         if event.type == pygame.KEYUP:
 
             if event.key == pygame.K_w:
                 controlsState["leftUp"] = False
             if event.key == pygame.K_s:
                 controlsState["leftDown"] = False
-                
+
             if event.key == pygame.K_UP:
                 controlsState["rightUp"] = False
             if event.key == pygame.K_DOWN:
                 controlsState["rightDown"] = False
 
+        # Quit if the user clicks the close button or closes it in any other way
         if event.type == pygame.QUIT:
             quitGame()
 
+    # Draw the current score (for both players) and the net
     drawScore()
-
     drawNet()
 
-    ball.move(windowWidth, windowHeight, leftBat, rightBat)
+    # Check if a point has been scored in the last ball.pauseTime ms. If so, the
+    # ball is drawn at the starting position, otherwise it is moved then drawn accordingly.
     if ball.scored and GAME_TIME.get_ticks() - ball.scoredTime < ball.pauseTime:
-        ball.draw(True)
+        ball.draw(True) # Draw the ball at the starting position
     else:
-        ball.draw(False)
+        ball.move(windowWidth, windowHeight, leftBat, rightBat)
+        ball.draw(False) # Draw the ball wherever it may be
 
+    # Move and draw the bats/paddles/whatever you want to call them
     leftBat.move(controlsState["leftUp"], controlsState["leftDown"], windowHeight)
     leftBat.draw()
 
     rightBat.move(controlsState["rightUp"], controlsState["rightDown"], windowHeight)
     rightBat.draw()
 
+    # Lock the fps to ~60 and update the display
     GAME_TIME.Clock().tick(60)
     pygame.display.update()
